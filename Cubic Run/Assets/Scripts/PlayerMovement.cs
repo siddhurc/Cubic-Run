f6a7871bc1094ab2d6daab4e71fe79fa8de9498c
siddhurc <siddhurc@gmail.com>
2024-05-04 20:58:12 -0400

    Modified TouchInputActions for detecting swipes.
    Also modified PlayerMovement script to debug and log the type of swipes in the console

--- Cubic Run/Assets/Scripts/PlayerMovement.cs
+++ Cubic Run/Assets/Scripts/PlayerMovement.cs
@@ -1,56 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float movement_speed = 0.5f;
 
-    public TouchActions touchActions;
-
-    public GameObject player;
-    private Rigidbody player_rb;
+    // Touch controls
+    private TouchInputActions touchInputActions;
+    [SerializeField] private float minimumSwipeMagnitude = 6f;
+    private Vector2 swipeStartPosition;
+    private bool isSwipeInProgress = false;
 
     private void Awake()
     {
-        touchActions = new TouchActions();
+        touchInputActions = new TouchInputActions();
     }
 
     private void OnEnable()
     {
-        touchActions.Enable();
-        //TouchSimulation.Enable();
+        touchInputActions.Enable();
     }
 
     private void OnDisable()
     {
-        touchActions.Disable();
+        touchInputActions.Disable();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        //horizontalMovement = new InputAction(binding: "HorizontalMovement/TouchPosition");
-        player_rb = player.GetComponent<Rigidbody>();
+        // Subscribe to swipe events
+        touchInputActions.TouchInputMovement.Swipe.started += ctx => swipeStartPosition = ctx.ReadValue<Vector2>();
+        touchInputActions.TouchInputMovement.Swipe.canceled += ctx => ProcessSwipe(ctx.ReadValue<Vector2>());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ProcessSwipe(Vector2 swipeEndPosition)
     {
-        float moveInput = touchActions.HorizontalMovement.TouchPosition.ReadValue<Vector2>().x;
+        if (!isSwipeInProgress)
+        {
+            Vector2 swipeDirection = swipeEndPosition - swipeStartPosition;
 
-        Debug.Log(moveInput);
+            // Check if swipe magnitude is greater than minimum threshold
+            if (swipeDirection.magnitude >= minimumSwipeMagnitude)
+            {
+                isSwipeInProgress = true;
 
-        Vector3 moveDirection = new Vector3(moveInput, 0f, 0f);
-
-        player.transform.Translate(moveDirection * Time.deltaTime * movement_speed);
-    }
-
-    private void MovePlayer(float movement)
-    {
-        player_rb.velocity = new Vector3(movement, 0f, 0f);
+                // Process swipe direction
+                if (Mathf.Abs(swipeDirection.x) > Mathf.Abs(swipeDirection.y))
+                {
+                    // Horizontal swipe
+                    if (swipeDirection.x > 0)
+                    {
+                        Debug.Log("Right swipe detected");
+                    }
+                    else
+                    {
+                        Debug.Log("Left swipe detected");
+                    }
+                }
+                else
+                {
+                    // Vertical swipe
+                    if (swipeDirection.y > 0)
+                    {
+                        Debug.Log("Upward swipe detected");
+                    }
+                    else
+                    {
+                        Debug.Log("Downward swipe detected");
+                    }
+                }
+            }
+        }
+        else
+        {
+            isSwipeInProgress = false;
+        }
     }
 }
-
